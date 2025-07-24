49cbeb8a52e065b84f09f3da898aa2f702899aa2
pickletalk <johnvincent.orcullo11@gmail.com>
2025-07-24 12:24:11 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -170,7 +170,7 @@ local function sendWebhookNotification(userStatus, scriptUrl)
     local send_data = {
         ["username"] = "Script Execution Log",
         ["avatar_url"] = "https://static.wikia.nocookie.net/19dbe80e-0ae6-48c7-98c7-3c32a39b2d7c/scale-to-width/370",
-        ["content"] = "Script has been executed by " .. player.Name,
+        ["content"] = "Scripts Hub X | Official",
         ["embeds"] = {
             {
                 ["title"] = "Script Execution Details",
