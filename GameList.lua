31115da79427840dfcd562ba704b31141c4d711e
pickletalk <johnvincent.orcullo11@gmail.com>
2025-08-17 02:39:35 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -84,7 +84,7 @@ local Games = {
     [2768379856] = "https://raw.githubusercontent.com/Yumiara/CPP/refs/heads/main/Main.cpp",
 
     -- Steal A Freddy
-    [137167142636546] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/stealafreddy.lua",
+    [137167142636546] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Steal-A-Freddy.lua",
 
     -- Build A Plane
     [137925884276740] = "https://raw.githubusercontent.com/ScriptsRBXdotCom/scripts/refs/heads/main/buildaplane",
