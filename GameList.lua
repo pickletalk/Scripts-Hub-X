eab4bfb38eb1790a67b9c63e2cb62c0a078b2877
pickletalk <johnvincent.orcullo11@gmail.com>
2025-09-01 09:54:10 +0800

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,103 +1,7 @@
 local Games = {   
-    -- Grow A Garden
-    [126884695634066] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua",
-    [91867617264223] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Grow%20a%20Garden.lua",
-  
-    -- Dig
-    [126244816328678] = "https://raw.githubusercontent.com/PlayzlxD0tmatter/DIG-SCRIPT-ZERO/refs/heads/main/dig.md",
-  
-    -- 99 Nights In A Forest
-    [79546208627805] = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua",
-    [126509999114328] = "https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua",
-  
-    -- Muscle Legends
-    [3623096087] = "https://raw.githubusercontent.com/Ceedif/Nw_Hub/refs/heads/main/Nw_Script",
-  
-    -- Arise Crossover
-    [87039211657390] = "https://raw.githubusercontent.com/thiennrb7/Script/refs/heads/main/Arise.txt",
-    [128336380114944] = "https://raw.githubusercontent.com/thiennrb7/Script/refs/heads/main/Arise.txt",
-    [116614712661486] = "https://raw.githubusercontent.com/thiennrb7/Script/refs/heads/main/Arise.txt",
-  
-    -- Blox Fruit
-    [2753915549] = "https://shorturl.at/f5xHw",
-    [4442272183] = "https://shorturl.at/f5xHw",
-    [7449423635] = "https://shorturl.at/f5xHw",
-  
-    -- Fisch
-    [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua",
-    [72907489978215] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua",
-  
-    -- The Strongest Battle Grounds
-    [10449761463] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/The%20Strongest%20Battleground.lua",
-  
-    -- Pets Go
-    [18901165922] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Pet%20GO.lua",
-  
-    -- Pet Simulator 99
-    [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
-    [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
-    [17503543197] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 3
-  
-    -- Dead Rails
-    [116495829188952] = "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/DeadRails",
-    [70876832253163] = "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/DeadRails",
-  
-    -- Steal a brainrot
-    [109983668079237] = "https://raw.githubusercontent.com/alexscriptsgg/Myopscript/refs/heads/main/myopscript",
-    [128762245270197] = "https://raw.githubusercontent.com/alexscriptsgg/Myopscript/refs/heads/main/myopscript",
-    
-    -- Murder Mystery 2
-    [142823291] = "https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2",
-    
-    -- Doors
-    [6516141723] = "https://raw.githubusercontent.com/KINGHUB01/BlackKing-obf/main/Doors%20Blackking%20And%20BobHub",
-    
-    -- piggy
-    [4623386862] = "https://raw.githubusercontent.com/StarHackScripts/StarHack-Hub-Deepstar-Hub/refs/heads/main/StarHack%20Hub%20-%20Piggy%20Pro%20Hub.txt",
-
-    -- All Star Tower Defense
-    [17687504411] = "https://raw.githubusercontent.com/bunnnwee/JimiHub/refs/heads/main/ASTDX-Normal",
-
-    -- Steal a Fish
-    [72212564918217] = "https://pastebin.com/raw/c6gQj1eR",
-
-    -- Garden Tower Defense
-    [108533757090220] = "https://raw.githubusercontent.com/NAVAAI098/Thunder-Hub/main/Loader.lua",
-
-    -- Ink Game
-    [125009265613167] = "https://raw.githubusercontent.com/wefwef127382/inkgames.github.io/refs/heads/main/ringta.lua",
-    [99567941238278] = "https://raw.githubusercontent.com/wefwef127382/inkgames.github.io/refs/heads/main/ringta.lua",
-
-    -- Untilted Boxing Game
-    [13621938427] = "https://raw.githubusercontent.com/pid4k/scripts/refs/heads/main/untitledboxinggame.lua",
-
-    -- Rivals
-    [17625359962] = "https://raw.githubusercontent.com/ByNami/RIVALS/main/RIVALS.lua",
-
-    -- Emergency Hamburg
-    [7711635737] = "https://raw.githubusercontent.com/Merdooon/skibidi-sigma-spec-ter/refs/heads/main/specter",
-
-    -- Steal a Pet
-    [74866348003583] = "https://raw.githubusercontent.com/Kyereal/Scripts/refs/heads/main/Steal%20a%20Pet%20😹",
-
-    -- 3008
-    [2768379856] = "https://raw.githubusercontent.com/Yumiara/CPP/refs/heads/main/Main.cpp",
-
     -- Steal A Freddy
     [137167142636546] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Steal-A-Freddy.lua",
 
-    -- Build A Plane
-    [137925884276740] = "https://raw.githubusercontent.com/ScriptsRBXdotCom/scripts/refs/heads/main/buildaplane",
-
-    -- Steal A Country
-    [107600241411943] = "https://raw.githubusercontent.com/gumanba/Scripts/main/StealaCountry",
-
-    -- Shrink Hide And Seek
-    [137541498231955] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/shrinkhideandseek.lua",
-
-    -- Troll Slapping Tower
-    [128829279907343] = "https://raw.githubusercontent.com/lotretscript/Lo-Tret-Script/refs/heads/main/Lo-Tret-Script.lua.txt",
-    
     -- Slap Tower
     [95508886069297] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua", -- 1
     [79089892790758] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua", -- 2
@@ -112,16 +16,10 @@ local Games = {
     [90922469229908] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua", -- 2
     [79223711715581] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua", -- 3
     [116270410750670] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua", -- 4
-
-    -- Tower Of Hell
-    [1962086868] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/GameScripts/Obbys.lua",
-
+    
     -- Troll Is A Pining Tower 2
     [83312952548612] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/troll%is%pinning%tower%2.lua",
-
-    -- Jailbreak
-    [606849621] = "https://api.luarmor.net/files/v4/loaders/b7281554d8cc27a19fa7c64e0a1dd0c1.lua",
-    [17190407811] = "https://api.luarmor.net/files/v4/loaders/b7281554d8cc27a19fa7c64e0a1dd0c1.lua"
 }
 
 return Games
+
