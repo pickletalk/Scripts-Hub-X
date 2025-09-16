26470bb6ea3c8afea945f13c986a8ac574049950
pickletalk <johnvincent.orcullo11@gmail.com>
2025-09-17 06:04:04 +0800

    Update Steal-A-Brainrot.lua

--- GameScripts/Steal-A-Brainrot.lua
+++ GameScripts/Steal-A-Brainrot.lua
@@ -15,7 +15,7 @@ local rootPart = character:WaitForChild("HumanoidRootPart")
 local platformEnabled = false
 local currentPlatform = nil
 local platformUpdateConnection = nil
-local PLATFORM_OFFSET = 3.46 -- Distance below player's feet (in studs)
+local PLATFORM_OFFSET = 3.465 -- Distance below player's feet (in studs)
 
 -- ESP variables
 local plotDisplays = {}
