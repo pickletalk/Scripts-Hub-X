30a4191c7ba7555cc81c1a0077416a7489bd6dcf
pickletalk <johnvincent.orcullo11@gmail.com>
2025-10-27 00:08:32 +0800

    Update URL for game script in TestGame.lua

--- TestGame.lua
+++ TestGame.lua
@@ -1,3 +1,5 @@
 return = {
-    [109983668079237] = ""
+    [109983668079237] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/Test.lua"
 }
+
+-- [[ FOR SHX DEVELOPER JUST CHANGE THE [] TO NEW GAME PROJECT ]]
