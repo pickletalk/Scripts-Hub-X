46ed0978376ac8890d1c7746058d2276df2ae83f
pickletalk <205507626+pickletalk@users.noreply.github.com>
2025-11-02 00:33:05 +0800

    Delete TestGame.lua
    Signed-off-by: pickletalk <205507626+pickletalk@users.noreply.github.com>

--- TestGame.lua
+++
@@ -1,5 +0,0 @@
-return {
-    [109983668079237] = "https://raw.githubusercontent.com/pickletalk/Scripts-Hub-X/refs/heads/main/Test.lua"
-}
-
--- [[ FOR SHX DEVELOPER JUST CHANGE THE [] TO NEW GAME PROJECT ]]
